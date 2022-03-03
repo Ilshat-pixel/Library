475f2729c23faba6b833ae431e1f1412a87432da
Ильшат Галиев <galiew.work@mail.ru>
2022-03-03 21:07:36 +0300

    Add base classes

---
+++ Library/Library.Domain/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Domain
+{
+    /// <summary>
+    /// 1.2.2 - Анемичная модель для БД Книги
+    /// </summary>
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Human Author { get; set; }
+        //TODO: думаю стоит выкинуть в отдельную таблицу жанры, пока не буду усложнять
+        public string Genre { get; set; }
+    }
+}
---
+++ Library/Library.Domain/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Library.Domain
+{
+    /// <summary>
+    /// 1.2.1 - Анемичная модель человека для БД
+    /// </summary>
+    public  class Human
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }  
+        public DateTime Birthday { get; set; } 
+    }
+}
